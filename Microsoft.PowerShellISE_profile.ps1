2644b20efb1f86b6f636a97544790a0ec13dcb5e
Kevin Hulquest <kevin.hulquest@netapp.com>
2018-08-18 13:27:31 -0600

    Updates for HCI dev environment on Windows

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,3 +1,7 @@
+<# The profile for PowerShell ISE lives in a different location than the normal PoSH CLI profile.
+ PS C:\Users\khulques> $profile
+ C:\Users\khulques\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+#>
 function Set-VsCmd
 {
     param(
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+<# The location for the user profile in PoSH is referenced by $profile and should be installed in the users Documents directory.
+PS C:\Users\khulques\src> $profile
+C:\Users\khulques\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#>
 function search-history {
     param([string] $lookfor)
     foreach ($cmd in $(get-history)) {
@@ -38,13 +42,6 @@ function prompt
 {
    $dirName = Get-Item .
 	"PS [$($(Get-Item .).Name)] > "
-}
-
-function Start-Derby 
-{
-    $block = {cd $env:derby_home/bin; ./startNetworkServer.bat}
-    $job = start-job -scriptblock $block
-    receive-job -job $job
 }
 
 function Get-Checksum($file, $crypto_provider) {
@@ -89,121 +86,76 @@ function Get-Version
     }
 
 }
-function sqlcmd
-{
-    & "c:\Program Files\Microsoft SQL Server\100\Tools\Binn\SQLCMD.EXE" -S bolkhulques03\HULQUEST -d StorageExplorer -w 100
-}
+
 function Get-Directories
 {
 	$dirs= Get-ChildItem | Where {$_.psIsContainer -eq $true}
 	$dirs |Select name
 }
-function To-AppAware
+
+$SrcRoot="c:/users/khulques/src"
+$AppRoot="c:/users/khulques/apps"
+
+function To-Src
 {
-	Set-Location c:/users/khulques/workspace
+	Set-Location $SrcRoot
 }
 
-function Sync-SVN
+function To-HCIMonitor
 {
-	$src="C:\_AppAware\SCOM\src"
-	$svn=$src+"\svn-clean"
-	$view= "\KMH_View\vob\IBP_Appaware\SCOM"
-	$ccrc=$src+$view
-	. c:\_AppAware\SCOM\src\Sync-CCRC.ps1 -svn $svn -cc $ccrc
+	Set-Location $SrcRoot/hci-monitor
 }
 
-function To-ClearCaseView
+function To-SFPrime
 {
-	Set-Location c:/_AppAware/SCOM/src/KMH_View/vob/IBP_Appaware/SCOM
+	Set-Location $SrcRoot/sfprime-hd
 }
 
-function To-RG
+function To-MNodeCfg
 {
-	Set-Location c:/_AppAware/SCOM/RecoveryGuru/SantricitySrc\SYMsm\SYMsm\devmgr\versioned\sam\recoveryguru
+	Set-Location $SrcRoot/hci-mnodecfg
 }
 
-function To-DCSF
-{
-	Set-Location c:/_AppAware/DCSF/src
-}
-
-function To-SCOM
-{
-	Set-Location c:/netapp/projects/scom
-}
-
-function To-SRC
-{
-	Set-Location c:/_AppAware/SSMS/SSMS-Trunk/Client
-}
-
-function To-SSMSI3
-{
-	Set-Location c:/_AppAware/SSMS/SSMS-I3
-}
-
-function To-SSMS 
-{
-	Set-Location c:/_AppAware/SSMS/SSMS-Trunk
-}
-
-function To-SYMbol
-{
-	Set-Location $sbma/SBMA_SYMbol_Adapter
-}
-
-function To-ProdConfig
-{
-	Set-Location $sbma/SBMA_Product_Config
-}
-
-function To-Tools
-{
-	Set-Location $sbma/SBMA_Tools
-}
 function To-Desktop
 {
 	Set-Location "c:/Documents And Settings/khulques/Desktop"
 }
+
 function Unix-LR
 { 
 	Get-ChildItem |Sort-Object -Property LastWriteTime
 }
+
 function Unix-L
 { 
 	Get-ChildItem |ft Name
 }
-#set-location c:/_appaware
+
+$VimVersion="80"
+$VimExe="c:/program files (x86)/vim/vim"+$VimVersion+"/gvim.exe"
+
 set-alias gh get-history
 set-alias which get-command
-set-alias vi "c:/program files (x86)/vim/vim74/gvim.exe"
+set-alias -name vi -value $VimExe
 set-alias ld Get-Directories
 set-alias lr Unix-LR
 set-alias ll Get-ChildItem 
 set-alias l Unix-L
 set-alias grep select-string
-set-alias src To-SRC
-set-alias trunk To-SSMS
-set-alias i3 To-SSMSI3
 set-alias find get-childitem
-$env:path += ";c:\program files\microsoft\visual studio 9.0\vc\bin;c:\ant\bin"
-$env:path += ";c:\windows\microsoft.net\framework\v3.5"
-$env:path += ";e:/bin/scripts/posh"
-$env:psmodulepath += ";C:\Program Files\NetApp\Modules\NetApp.SANtricity.PowerShell"
-$sbma="C:\users\khulques\workspace\khulques_IBP_SBMA_EXT_EAGLE6.0_Dev_Work\vobs\app_SBMA"
+$env:path += ";"+$AppRoot+"/ctags58"
+#$env:path += ";c:\windows\microsoft.net\framework\v3.5"
+#$env:path += ";e:/bin/scripts/posh"
+#$env:psmodulepath += ";C:\Program Files\NetApp\Modules\NetApp.SANtricity.PowerShell"
+#$sbma="C:\users\khulques\workspace\khulques_IBP_SBMA_EXT_EAGLE6.0_Dev_Work\vobs\app_SBMA"
 
-set-alias scom To-Scom 
-set-alias dcsf To-DCSF 
-set-alias rg To-RG
-set-alias ws To-AppAware 
-set-alias desktop To-Desktop 
-set-alias symbol To-SYMbol 
-set-alias pc To-ProdConfig
-set-alias tools To-Tools 
-set-alias ccrc To-ClearCaseView
+set-alias sfp To-SFPrime
+set-alias hci To-HCIMonitor 
+set-alias mnode To-MNodeCfg
+set-alias src To-SRC
+
 set-alias ih Invoke-History
 set-alias sh Search-History
-set-alias as Add-PSSnapin
 remove-item alias:cd -errora silentlycontinue
 remove-item -Force alias:diff -errora silentlycontinue
-set-item -path env:SVN_EDITOR -value "vi"
+set-item -path env:GIT_EDITOR -value "vi"

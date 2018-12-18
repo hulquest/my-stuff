9f4bfb3f694842a654f740d0a34f8ebe8f022eaf
Kevin Hulquest <khulques@netapp.com>
2018-12-18 16:48:02 -0700

    Adding mount point for other scripts in the container.

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source py35-sfp/bin/activate
 bin/get_docker_image.py
-docker run --rm -it -v ~/src/projects/sfprime/system_test:/sfprime-auto/ -v ~/.ssh:/root/.ssh docker.sqd.solidfire.net:9000/sfprime-auto:latest
+docker run --rm -it -v ~/scripts:/sfprime-auto/kevin -v ~/src/projects/sfprime/system_test:/sfprime-auto/ -v ~/.ssh:/root/.ssh docker.sqd.solidfire.net:9000/sfprime-auto:latest
